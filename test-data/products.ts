2f3bd8281a9a561933a67048eb454b6bb7289f7a
Anna Jagieła <a.jagiela96@gmail.com>
2026-02-22 15:25:39 +0100

    update snapshots in CI

--- test-data/products.ts
+++ test-data/products.ts
@@ -5,7 +5,7 @@
 } as const; */
 
 export const PRODUCTS = {
-  GREY_JACKET: { label: 'Grey jacket', price: "£55.00" },
-  NOIR_JACKET: { label: 'Noir jacket', price: "£60.00" },
-  STRIPED_TOP: { label: 'Striped top', price: "£50.00" }
+  GREY_JACKET: { label: 'Grey jacket', price: "£55.00", variantId: "611945025"},
+  NOIR_JACKET: { label: 'Noir jacket', price: "£60.00", variantId: "611952521" },
+  STRIPED_TOP: { label: 'Striped top', price: "£50.00", variantId: "611951029" }
 } as const;
--- tests/api/add-to-cart.spec.ts
+++ tests/api/add-to-cart.spec.ts
@@ -2,33 +2,27 @@ import { expect, test } from '../../fixtures/test-fixtures';
 import { PRODUCTS } from '../../test-data/products';
 
 test('API + UI - Add to cart makes correct API call', async ({ page, homePage, productPage, cartPage }) => {
-  // Set up API request listener BEFORE the action
+  const testProduct = PRODUCTS.NOIR_JACKET
   const cartApiPromise = page.waitForRequest(request => 
     request.url().includes('/cart/add.js') && 
     request.method() === 'POST'
   );
   
-  // Perform the action
+  
   await homePage.goto();
   await homePage.openProductByName(PRODUCTS.NOIR_JACKET.label);
   await productPage.addToCart();
-  
-  // Wait for and capture the API request
   const cartApiRequest = await cartApiPromise;
   
-  // ✅ Verify API request payload
   const payload = cartApiRequest.postDataJSON();
   expect(payload).toHaveProperty('id');
-  expect(payload.id).toBe('611952521'); // Noir jacket variant ID
+  expect(payload.id).toBe(testProduct.variantId); // Noir jacket variant ID
   
-  // ✅ Verify API response
+  
   const response = await cartApiRequest.response();
   const responseBody = await response?.json();
-  
   expect(response?.status()).toBe(200);
   expect(responseBody.product_title).toBe('Noir jacket');
   expect(responseBody.quantity).toBeGreaterThan(0);
-  
-  // ✅ Verify UI updated
   await expect(cartPage.cartCountElement).toContainText('(1)');
 });
